9317cc85ef3cdf0421a260668ca7e656e135a599
muneeb52 <bcsf15m030@pucit.edu.pk>
2018-04-29 16:03:07 +0500

    add entity and dao classes

---
+++ User Management System/User Management System/Models/admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace User_Management_System.Models
+{
+    public class admin
+    {
+        public int adminId { get; set; }
+        public string adminName { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ User Management System/User Management System/Models/user.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace User_Management_System.Models
+{
+    public class user
+    {
+        public String name { get; set; }
+        public String login { get; set; }
+        public String password { get; set; }
+        public String email { get; set; }
+        public String gender { get; set; }
+        public String address { get; set; }
+        public int age { get; set; }
+        public String nic { get; set; }
+        public DateTime dob { get; set; }
+        public bool isCricket { get; set; }
+        public bool isHockey { get; set; }
+        public bool isChess { get; set; }
+        public String imageName { get; set; }
+
+    }
+}
