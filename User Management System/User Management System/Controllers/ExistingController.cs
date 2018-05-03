78725b6f7a2f3d267af9fa99220f3cddc4ba6a0e
muneeb52 <bcsf15m030@pucit.edu.pk>
2018-05-03 20:54:30 +0500

    working on existing module

--- User Management System/User Management System/Controllers/ExistingController.cs
+++ User Management System/User Management System/Controllers/ExistingController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using User_Management_System.Models;
 
 namespace User_Management_System.Controllers
 {
@@ -10,8 +11,23 @@ namespace User_Management_System.Controllers
     {
         //
         // GET: /Existing/
-        public ActionResult Index()
+        public ActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(user userObj)
+        {
+            userDAO userObjDao = new userDAO();
+            String email=userObjDao.validateUser(userObj);
+            if (email!=null)
+                return Redirect("/home/home?email=" + email);
+            else
+            {
+                ViewBag.txtLogin = userObj.txtLogin;
+                ViewBag.msg = "Incorrect Info";
+            }
             return View();
         }
 	}
--- User Management System/User Management System/Models/userDAO.cs
+++ User Management System/User Management System/Models/userDAO.cs
@@ -144,5 +144,29 @@ namespace User_Management_System.Models
                 return userList;
             }
         }
+
+        public String validateUser(user userObj)
+        {
+            String connString = @"Data Source=.\SQLEXPRESS2012; Initial Catalog=Assignment4; Integrated Security=True; Persist Security Info=True;";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                String sqlQuery = String.Format(@"Select * from dbo.users where login=@Login and password=@Password");
+                SqlCommand command = new SqlCommand(sqlQuery, conn);
+                SqlParameter param = command.Parameters.AddWithValue("@Login", userObj.txtLogin);
+                if (userObj.txtLogin == null)
+                    param.Value = DBNull.Value;
+                param = command.Parameters.AddWithValue("@Password", userObj.txtPassword);
+                if (userObj.txtPassword == null)
+                    param.Value = DBNull.Value;
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    if(reader.Read())
+                        return reader.GetString(reader.GetOrdinal("email"));
+                }
+            }
+            return null;
+        }
     }
 }
